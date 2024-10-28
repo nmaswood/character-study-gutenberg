59f1d127843c1c6efc312e630d83806dc6910319
kidusleo <kidusleo@gmail.com>
2024-10-28 19:24:46 +0300

    Refactored useBookDialog

--- src/app/hooks/useBookDialog.ts
+++ src/app/hooks/useBookDialog.ts
@@ -1,14 +1,65 @@
 "use client";
 
 import { LoadedBook } from "@/stores/useBookStore";
+import { useBookUIStore } from "@/stores/useBookUIStore";
 import axios from "axios";
+import { encode } from "gpt-tokenizer";
+import { useMemo, useState } from "react";
+import { AnalyzeBookResponse } from "../api/model/book/analyze/route";
 
 export default function useBookDialog() {
-    const sendBook = async (book: LoadedBook) => {
-        const response = await axios.post("/api/model", book);
+    const { openedBook, closeBook, setOpenedBook } = useBookUIStore();
+    const [isAnalyzing, setIsAnalyzing] = useState(false);
 
-        console.log({ data: response.data });
+    const analyzeBook = async (book: LoadedBook) => {
+        setIsAnalyzing(true);
+
+        try {
+            const response = await axios.post("/api/model/book/analyze", book);
+            const bookAnalysis: AnalyzeBookResponse = response.data;
+
+            if (openedBook) {
+                setOpenedBook({
+                    ...openedBook,
+                    isAnalyzed: true,
+                    characters: bookAnalysis.characters,
+                    shortSummary: bookAnalysis.shortSummary,
+                });
+            }
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                console.log({
+                    error: {
+                        cause: error.cause,
+                        code: error.code,
+                    },
+                });
+            }
+        } finally {
+            setIsAnalyzing(false);
+        }
     };
 
-    return { sendBook };
+    const totalCount = useMemo(() => {
+        if (!openedBook) return [0, 0];
+        const bookContent = openedBook.bookContent;
+        const tokenCount = encode(bookContent).length;
+        const wordCount = bookContent.split(" ").length;
+
+        return [wordCount, tokenCount];
+    }, [openedBook]);
+
+    const onOpenChange = (open: boolean) => {
+        if (!open) {
+            closeBook();
+        }
+    };
+
+    return {
+        analyzeBook,
+        totalCount,
+        onOpenChange,
+        openedBook,
+        isAnalyzing,
+    };
 }
