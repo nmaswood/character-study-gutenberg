ff1221dc38c73135bdf87627728cef8b99e102e2
kidusleo <kidusleo@gmail.com>
2024-10-28 19:23:00 +0300

    Refactored analyze page

--- src/app/api/model/book/analyze/route.ts
+++ src/app/api/model/book/analyze/route.ts
@@ -2,10 +2,11 @@ import { genAI } from "@/gemini";
 import { BookAnalysisSchema } from "@/gemini/schema";
 import { prisma } from "@/prisma/client";
 import { LoadedBook } from "@/stores/useBookStore";
+import { HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 import { Character, CharactersOnEvents, PlotEvent } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-type BookAnalysisResponse = {
+type BookAnalysisGeminiResponse = {
     characters: {
         characterName: string;
         traits: string[];
@@ -21,16 +22,32 @@ type BookAnalysisResponse = {
     shortSummary: string;
 };
 
-export async function POST(request: Request) {
-    const book: LoadedBook = await request.json();
+export type AnalyzeBookResponse = {
+    shortSummary: string;
+    characters: string[];
+};
 
-    const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
-        generationConfig: {
-            responseMimeType: "application/json",
-            responseSchema: BookAnalysisSchema,
-        },
-        systemInstruction: `You will receive a book in JSON format under the 'bookContent' field. 
+// The error happens even if safety settings are set to block none.
+const safetySettings = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+];
+
+const instructions = `You will receive a PUBLIC DOMAIN (NO COPYRIGHT APPLIES) book in JSON format under the 'bookContent' field. 
         Your task is to:
         1. Identify all relevant and significant characters (maximum of five) within the content. For each character, extract and return the following details (maximum of 5 each):
         - Full name of the character.
@@ -48,118 +65,132 @@ export async function POST(request: Request) {
 
         Organize the analysis with precision and depth, ensuring the extracted details are comprehensive, concise, and contextually relevant.
 
-        Return the analysis in JSON format in accordance with the schema.`,
+        Return the analysis in JSON format in accordance with the schema.`;
+
+export async function POST(request: Request) {
+    const book: LoadedBook = await request.json();
+
+    const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-flash",
+        generationConfig: {
+            responseMimeType: "application/json",
+            responseSchema: BookAnalysisSchema,
+        },
+        safetySettings,
+        systemInstruction: instructions,
     });
 
-    const result = await model.generateContent(JSON.stringify(book));
-    const response = result.response.text();
+    try {
+        const result = await model.generateContent(JSON.stringify(book));
+        const response = result.response.text();
 
-    const data: BookAnalysisResponse = JSON.parse(response);
+        const data: BookAnalysisGeminiResponse = JSON.parse(response);
 
-    const characters: Omit<Character, "id" | "bookId">[] = data.characters.map(
-        (character) => ({
-            characterName: character.characterName,
-            goals: character.goals,
-            relationships: character.relationships,
-            traits: character.traits,
-            tone: character.tone,
-            quotes: character.quotes,
-        })
-    );
+        const characters: Omit<Character, "id" | "bookId">[] =
+            data.characters.map((character) => ({
+                characterName: character.characterName,
+                goals: character.goals,
+                relationships: character.relationships,
+                traits: character.traits,
+                tone: character.tone,
+                quotes: character.quotes,
+            }));
 
-    const plotEvents: Omit<PlotEvent, "id" | "bookId">[] = data.plotEvents.map(
-        (plotEvent) => ({
-            eventSummary: plotEvent.eventSummary,
-        })
-    );
+        const plotEvents: Omit<PlotEvent, "id" | "bookId">[] =
+            data.plotEvents.map((plotEvent) => ({
+                eventSummary: plotEvent.eventSummary,
+            }));
 
-    const analysis = await prisma.$transaction(
-        async () => {
-            const savedAnalysis = await prisma.book.create({
-                data: {
-                    id: book.id,
-                    shortSummary: data.shortSummary,
-                    characters: {
-                        createMany: {
-                            data: characters,
+        const analysis = await prisma.$transaction(
+            async () => {
+                const savedAnalysis = await prisma.book.create({
+                    data: {
+                        id: book.id,
+                        shortSummary: data.shortSummary,
+                        characters: {
+                            createMany: {
+                                data: characters,
+                            },
+                        },
+                        plotEvents: {
+                            createMany: {
+                                data: plotEvents,
+                            },
                         },
                     },
-                    plotEvents: {
-                        createMany: {
-                            data: plotEvents,
-                        },
+                    // Include the characters, plot events, and characters on events in the response.
+                    include: {
+                        characters: true,
+                        plotEvents: true,
                     },
-                },
-                // Include the characters, plot events, and characters on events in the response.
-                include: {
-                    characters: true,
-                    plotEvents: true,
-                },
-            });
+                });
 
-            // Get the characters and plot events from the saved analysis.
-            const charactersOnEvents: CharactersOnEvents[] =
-                savedAnalysis.plotEvents.reduce(
-                    (prev: CharactersOnEvents[], event) => {
-                        const charactersInEvent = data.plotEvents.find(
-                            (pEvent) =>
-                                pEvent.eventSummary === event.eventSummary
-                        )?.charactersInEvent;
+                // Get the characters and plot events from the saved analysis.
+                const charactersOnEvents: CharactersOnEvents[] =
+                    savedAnalysis.plotEvents.reduce(
+                        (prev: CharactersOnEvents[], event) => {
+                            const charactersInEvent = data.plotEvents.find(
+                                (pEvent) =>
+                                    pEvent.eventSummary === event.eventSummary
+                            )?.charactersInEvent;
 
-                        if (charactersInEvent) {
-                            // Get the characters in the event and create a new entry in the characters on events table for each character.
-                            const currCharactersOnEvent: CharactersOnEvents[] =
-                                savedAnalysis.characters.reduce(
-                                    (
-                                        prev: CharactersOnEvents[],
-                                        currCharacter
-                                    ) => {
-                                        if (
-                                            charactersInEvent.includes(
-                                                currCharacter.characterName
+                            if (charactersInEvent) {
+                                // Get the characters in the event and create a new entry in the characters on events table for each character.
+                                const currCharactersOnEvent: CharactersOnEvents[] =
+                                    savedAnalysis.characters.reduce(
+                                        (
+                                            prev: CharactersOnEvents[],
+                                            currCharacter
+                                        ) => {
+                                            if (
+                                                charactersInEvent.includes(
+                                                    currCharacter.characterName
+                                                )
                                             )
-                                        )
-                                            return [
-                                                ...prev,
-                                                {
-                                                    // Link the character to the plot event.
-                                                    plotEventId: event.id,
-                                                    characterId:
-                                                        currCharacter.id,
-                                                },
-                                            ];
+                                                return [
+                                                    ...prev,
+                                                    {
+                                                        // Link the character to the plot event.
+                                                        plotEventId: event.id,
+                                                        characterId:
+                                                            currCharacter.id,
+                                                    },
+                                                ];
 
-                                        return prev;
-                                    },
-                                    []
-                                );
+                                            return prev;
+                                        },
+                                        []
+                                    );
 
-                            // Add the new characters on events to the list.
-                            return [...prev, ...currCharactersOnEvent];
-                        }
+                                // Add the new characters on events to the list.
+                                return [...prev, ...currCharactersOnEvent];
+                            }
 
-                        return prev;
-                    },
-                    []
-                );
+                            return prev;
+                        },
+                        []
+                    );
 
-            await prisma.charactersOnEvents.createMany({
-                data: charactersOnEvents,
-            });
+                await prisma.charactersOnEvents.createMany({
+                    data: charactersOnEvents,
+                });
 
-            return savedAnalysis;
-        },
-        {
-            timeout: 10000,
-        }
-    );
+                return savedAnalysis;
+            },
+            {
+                timeout: 10000,
+            }
+        );
 
-    return NextResponse.json({
-        data: {
+        return NextResponse.json({
             shortSummary: analysis.shortSummary,
             characters: analysis.characters.map(
                 (character) => character.characterName
             ),
-        },
-    });
+        });
+    } catch (error) {
+        console.error(error);
+
+        return NextResponse.json(error, { status: 500 });
+    }
 }
