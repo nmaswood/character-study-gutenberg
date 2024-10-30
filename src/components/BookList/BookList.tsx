51dfec0030396e146b546a717004dcc232b9f888
kidusleo <kidusleo@gmail.com>
2024-10-31 01:50:24 +0300

    Handled loading state better

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -6,35 +6,33 @@ import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components
 import BookListItem from "./BookListItem";
 
 export default function BookList() {
-  const { localBooks, removeBook, _hasHydrated } = useBookStore();
+	const { localBooks, removeBook, _hasHydrated } = useBookStore();
 
-  if (!_hasHydrated) return <>loading</>;
-
-  return (
-    <div className="z-10 min-h-[300px] divide-y divide-solid rounded-t-lg md:px-10 md:py-8">
-      <div className="flex flex-row items-center justify-between pb-2">
-        <div className="text-xl font-bold">Local Library</div>
-        <div>{localBooks.length} book(s) found</div>
-      </div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="hidden whitespace-nowrap md:table-cell lg:table-cell">ID</TableHead>
-            <TableHead className="min-w-[120px] md:min-w-[150px] lg:w-[200px]">Title</TableHead>
-            <TableHead className="min-w-[120px] md:min-w-[150px] lg:w-[200px]">Author</TableHead>
-            <TableHead className="hidden whitespace-nowrap md:table-cell lg:table-cell">Saved Date</TableHead>
-            <TableHead className="hidden whitespace-nowrap md:table-cell lg:table-cell">Language</TableHead>
-            <TableHead className="hidden whitespace-nowrap text-right md:table-cell lg:table-cell"></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {!_hasHydrated ? (
-            <>loading</>
-          ) : (
-            localBooks.map((book) => <BookListItem key={book.id} book={book} onDelete={removeBook} />)
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  );
+	return (
+		<div className="z-10 min-h-[300px] divide-y divide-solid rounded-t-lg md:px-10 md:py-8">
+			<div className="flex flex-row items-center justify-between pb-2">
+				<div className="text-xl font-bold">Local Library</div>
+				<div>{localBooks.length} book(s) found</div>
+			</div>
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead className="hidden whitespace-nowrap md:table-cell lg:table-cell">ID</TableHead>
+						<TableHead className="min-w-[120px] md:min-w-[150px] lg:w-[200px]">Title</TableHead>
+						<TableHead className="min-w-[120px] md:min-w-[150px] lg:w-[200px]">Author</TableHead>
+						<TableHead className="hidden whitespace-nowrap md:table-cell lg:table-cell">Saved Date</TableHead>
+						<TableHead className="hidden whitespace-nowrap md:table-cell lg:table-cell">Language</TableHead>
+						<TableHead className="hidden whitespace-nowrap text-right md:table-cell lg:table-cell"></TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{!_hasHydrated ? (
+						<></>
+					) : (
+						localBooks.map((book) => <BookListItem key={book.id} book={book} onDelete={removeBook} />)
+					)}
+				</TableBody>
+			</Table>
+		</div>
+	);
 }
