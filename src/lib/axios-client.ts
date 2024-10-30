15be2478107acaf4a862173d9fca837488bb904e
kidusleo <kidusleo@gmail.com>
2024-10-31 01:22:10 +0300

    Fixed build issues

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -1,7 +1,4 @@
 import axios from "axios";
-import { wrapper } from "axios-cookiejar-support";
-import tough from "tough-cookie";
 
 // Initialize a cookie jar
-const cookieJar = new tough.CookieJar();
-export const axiosClient = wrapper(axios.create({ jar: cookieJar }));
+export const axiosClient = axios.create();
