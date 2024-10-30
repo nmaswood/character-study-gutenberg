46274e3cc1a00b79ed2446daf8c139bf50191f86
kidusleo <kidusleo@gmail.com>
2024-10-30 16:11:34 +0300

    Refactored page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
+"use client";
+
 import BookList from "@/components/BookList";
 import FetchBookInput from "../components/FetchBook/FetchBookInput";
 import { Toaster } from "sonner";
+import BookDialog from "@/components/BookDialog";
 
 export default function Home() {
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start border-4">
-                <FetchBookInput />
-                <BookList />
-                <Toaster />
-            </main>
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
-        </div>
-    );
+  return (
+    <div className="absolute inset-0 z-[-2] bg-white dark:bg-neutral-950">
+      <div className="absolute inset-0 h-full w-full bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)] dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+        {/* Main content aligned towards the top */}
+        <main className="flex h-full w-full flex-col items-center justify-start gap-8 p-8 pt-10 sm:items-center">
+          <div className="flex w-full max-w-screen-sm justify-center pt-10">
+            <FetchBookInput />
+          </div>
+          <div className="w-full max-w-screen-lg">
+            <BookList />
+            <BookDialog />
+          </div>
+          <Toaster />
+        </main>
+      </div>
+    </div>
+  );
 }
