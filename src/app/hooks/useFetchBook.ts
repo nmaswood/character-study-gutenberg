d1d0da7b561afd833f1825c811135dee5429cf85
kidusleo <kidusleo@gmail.com>
2024-10-30 16:35:29 +0300

    Styled usefetch

--- src/app/hooks/useFetchBook.ts
+++ src/app/hooks/useFetchBook.ts
@@ -3,161 +3,157 @@ import { axiosClient } from "@/lib/axios-client";
 import { Book, LoadedBook, useBookStore } from "@/stores/useBookStore";
 import { useBookUIStore } from "@/stores/useBookUIStore";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { FetchedBook } from "../../stores/useBookStore";
 import { AnalyzeBookResponse } from "../api/model/book/analyze/route";
 import { Character } from "./useBookDialog";
 
 export default function useFetchBook() {
-    const [bookId, setBookId] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [loadingBookContent, setLoadingBookContent] = useState(false);
+  const [bookId, setBookId] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [loadingBookContent, setLoadingBookContent] = useState(false);
 
-    const { setOpenedBook } = useBookUIStore();
+  const { setOpenedBook } = useBookUIStore();
 
-    const { localBooks, addBook, getBook, isLoadedBook, loadBookContent } =
-        useBookStore();
+  const { localBooks, addBook, getBook, isLoadedBook, loadBookContent } = useBookStore();
 
-    const fetchBookMetadata = async () => {
-        setLoading(true);
+  useEffect(() => {
+    if (!loading) setBookId("");
+  }, [loading]);
 
-        try {
-            let book: Book | FetchedBook | undefined = localBooks.find(
-                (book) => book.id === bookId
-            );
+  const fetchBookMetadata = async () => {
+    setLoading(true);
 
-            if (book) {
-                toast.info("Book already in library");
-                // let loadedBook: LoadedBook = await fetchBookContent(bookId);
-                await openBook(bookId);
-                return;
-            }
+    try {
+      let book: Book | FetchedBook | undefined = localBooks.find((book) => book.id === bookId);
 
-            const response = await axios.get(`/api/book/${bookId}/metadata`);
+      if (book) {
+        toast.info("Book already in library");
+        // let loadedBook: LoadedBook = await fetchBookContent(bookId);
+        await openBook(bookId);
+        return;
+      }
 
-            if (response.status === 404) {
-                toast.error("Book could not be found");
-                return;
-            }
+      const response = await axios.get(`/api/book/${bookId}/metadata`);
 
-            book = response.data as FetchedBook;
+      if (response.status === 404) {
+        toast.error("Book could not be found");
+        return;
+      }
 
-            if (book) {
-                toast.success("Book successfully fetched");
+      book = response.data as FetchedBook;
 
-                addBook(book);
-            }
-        } catch (error) {
-            toast.error("Something went wrong");
+      if (book) {
+        toast.success("Book successfully fetched");
 
-            if (axios.isAxiosError(error)) {
-                console.error(error);
-            }
-        } finally {
-            setLoading(false);
-        }
-    };
+        addBook(book);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
 
-    const fetchBookContent = async (bookId: string): Promise<string> => {
-        try {
-            const bookContent = await axiosClient
-                .get("/api/book/" + bookId)
-                .then((response) => response.data);
-            return bookContent;
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.error({
-                    error: {
-                        cause: error.cause,
-                        code: error.code,
-                    },
-                });
-            }
+      if (axios.isAxiosError(error)) {
+        console.error(error);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
-            throw new Error("Error: could not load book");
-        }
-    };
+  const fetchBookContent = async (bookId: string): Promise<string> => {
+    try {
+      const bookContent = await axiosClient.get("/api/book/" + bookId).then((response) => response.data);
+      return bookContent;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error({
+          error: {
+            cause: error.cause,
+            code: error.code,
+          },
+        });
+      }
 
-    const checkIfBookIsAnalyzed = async (
-        bookId: string
-    ): Promise<{
-        isAnalyzed: boolean;
-        characters?: Character[];
-        shortSummary?: string;
-    }> => {
-        {
-            try {
-                const analyzedBook: AnalyzeBookResponse = (
-                    await axios.get("/api/model/book/" + bookId)
-                ).data;
+      throw new Error("Error: could not load book");
+    }
+  };
 
-                return {
-                    isAnalyzed: true,
-                    characters: analyzedBook.characters,
-                    shortSummary: analyzedBook.shortSummary,
-                };
-            } catch (error) {
-                if (axios.isAxiosError(error)) {
-                    console.log({
-                        error: {
-                            cause: error.cause,
-                            code: error.code,
-                            message: "Book is not analyzed",
-                        },
-                    });
-                    if (error.code === "ERR_BAD_RESPONSE") {
-                        throw new Error(error.message);
-                    }
-                }
+  const checkIfBookIsAnalyzed = async (
+    bookId: string,
+  ): Promise<{
+    isAnalyzed: boolean;
+    characters?: Character[];
+    shortSummary?: string;
+  }> => {
+    {
+      try {
+        const analyzedBook: AnalyzeBookResponse = (await axios.get("/api/model/book/" + bookId)).data;
 
-                return { isAnalyzed: false };
-            }
-        }
-    };
-
-    const openBook = async (bookId: string) => {
-        setLoadingBookContent(true);
-
-        const book = getBook(bookId);
-        let loadedBook: LoadedBook;
-
-        if (!isLoadedBook(book)) {
-            const bookContent = await fetchBookContent(bookId);
-            loadedBook = loadBookContent(bookId, bookContent);
-        } else {
-            loadedBook = book;
+        return {
+          isAnalyzed: true,
+          characters: analyzedBook.characters,
+          shortSummary: analyzedBook.shortSummary,
+        };
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          console.log({
+            error: {
+              cause: error.cause,
+              code: error.code,
+              message: "Book is not analyzed",
+            },
+          });
+          if (error.code === "ERR_BAD_RESPONSE") {
+            throw new Error(error.message);
+          }
         }
 
-        try {
-            const { isAnalyzed, characters, shortSummary } =
-                await checkIfBookIsAnalyzed(bookId);
-            if (isAnalyzed) {
-                setOpenedBook({
-                    ...loadedBook,
-                    isAnalyzed: true,
-                    characters: characters,
-                    shortSummary: shortSummary,
-                });
-            } else setOpenedBook(loadedBook);
-        } catch (err) {
-            console.error(err);
-        }
+        return { isAnalyzed: false };
+      }
+    }
+  };
 
-        setLoadingBookContent(false);
-    };
+  const openBook = async (bookId: string) => {
+    setLoadingBookContent(true);
 
-    const onBookIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setBookId(e.target.value);
-    };
+    const book = getBook(bookId);
+    let loadedBook: LoadedBook;
 
-    return {
-        bookId,
-        loading,
-        loadingBookContent,
-        openBook,
-        onBookIdChange,
-        fetchBookMetadata,
-        fetchBookContent,
-    };
+    if (!isLoadedBook(book)) {
+      const bookContent = await fetchBookContent(bookId);
+      loadedBook = loadBookContent(bookId, bookContent);
+    } else {
+      loadedBook = book;
+    }
+
+    try {
+      const { isAnalyzed, characters, shortSummary } = await checkIfBookIsAnalyzed(bookId);
+      if (isAnalyzed) {
+        setOpenedBook({
+          ...loadedBook,
+          isAnalyzed: true,
+          characters: characters,
+          shortSummary: shortSummary,
+        });
+      } else setOpenedBook(loadedBook);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setLoadingBookContent(false);
+  };
+
+  const onBookIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBookId(e.target.value);
+  };
+
+  return {
+    bookId,
+    loading,
+    loadingBookContent,
+    openBook,
+    onBookIdChange,
+    fetchBookMetadata,
+    fetchBookContent,
+  };
 }
