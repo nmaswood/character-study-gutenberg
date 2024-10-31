1d098f194fcc2114592a359c5bc15f8e688f97d2
kidusleo <kidusleo@gmail.com>
2024-10-31 15:05:40 +0300

    Refactored useBookDialog

--- src/app/hooks/useBookDialog.ts
+++ src/app/hooks/useBookDialog.ts
@@ -6,83 +6,90 @@ import axios from "axios";
 import { encode } from "gpt-tokenizer";
 import { useMemo, useState } from "react";
 import { AnalyzeBookResponse } from "../api/model/book/analyze/route";
+import { toast } from "sonner";
 
 export type Character = {
-  id: number;
-  characterName: string;
-  quotes: string[];
+	id: number;
+	characterName: string;
+	quotes: string[];
 };
 
 export default function useBookDialog() {
-  const { openedBook, closeBook, setOpenedBook } = useBookUIStore();
-  const [isAnalyzing, setIsAnalyzing] = useState(false);
-  const [isChatOpen, setIsChatOpen] = useState(false);
-  const [activeCharacter, setActiveCharacter] = useState<Character | null>(null);
+	const { openedBook, closeBook, setOpenedBook } = useBookUIStore();
+	const [isAnalyzing, setIsAnalyzing] = useState(false);
+	const [isChatOpen, setIsChatOpen] = useState(false);
+	const [activeCharacter, setActiveCharacter] = useState<Character | null>(null);
 
-  const handleChatOpen = (character: Character) => {
-    setActiveCharacter(character);
-    setIsChatOpen(true);
-  };
+	const handleChatOpen = (character: Character) => {
+		setActiveCharacter(character);
+		setIsChatOpen(true);
+	};
 
-  const handleCloseChat = () => {
-    setIsChatOpen(false);
-    setActiveCharacter(null);
-  };
+	const handleCloseChat = () => {
+		setIsChatOpen(false);
+		setActiveCharacter(null);
+	};
 
-  const analyzeBook = async (book: LoadedBook) => {
-    setIsAnalyzing(true);
+	const analyzeBook = async (book: LoadedBook) => {
+		setIsAnalyzing(true);
 
-    try {
-      const response = await axios.post("/api/model/book/analyze", book);
-      const bookAnalysis: AnalyzeBookResponse = response.data;
+		try {
+			const response = await axios.post("/api/model/book/analyze", book);
+			const bookAnalysis: AnalyzeBookResponse = response.data;
 
-      if (openedBook) {
-        setOpenedBook({
-          ...openedBook,
-          isAnalyzed: true,
-          characters: bookAnalysis.characters,
-          shortSummary: bookAnalysis.shortSummary,
-        });
-      }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        console.error({
-          error: {
-            cause: error.cause,
-            code: error.code,
-          },
-        });
-      }
-    } finally {
-      setIsAnalyzing(false);
-    }
-  };
+			if (openedBook) {
+				setOpenedBook({
+					...openedBook,
+					isAnalyzed: true,
+					characters: bookAnalysis.characters,
+					shortSummary: bookAnalysis.shortSummary,
+				});
+			}
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				console.error({
+					error: {
+						cause: error.cause,
+						code: error.code,
+					},
+				});
 
-  const totalCount = useMemo(() => {
-    if (!openedBook) return [0, 0];
-    const bookContent = openedBook.bookContent;
-    const tokenCount = encode(bookContent).length;
-    const wordCount = bookContent.split(" ").length;
+				if (error.status === 500 && error.response?.data.statusText === "Too Many Requests") {
+					toast.error("Uh oh", {
+						description: "Too many requests, please try again later.",
+					});
+				}
+			}
+		} finally {
+			setIsAnalyzing(false);
+		}
+	};
 
-    return [wordCount, tokenCount];
-  }, [openedBook]);
+	const totalCount = useMemo(() => {
+		if (!openedBook) return [0, 0];
+		const bookContent = openedBook.bookContent;
+		const tokenCount = encode(bookContent).length;
+		const wordCount = bookContent.split(" ").length;
 
-  const onOpenChange = (open: boolean) => {
-    if (!open) {
-      closeBook();
-      handleCloseChat();
-    }
-  };
+		return [wordCount, tokenCount];
+	}, [openedBook]);
 
-  return {
-    activeCharacter,
-    analyzeBook,
-    handleChatOpen,
-    handleCloseChat,
-    isAnalyzing,
-    isChatOpen,
-    onOpenChange,
-    openedBook,
-    totalCount,
-  };
+	const onOpenChange = (open: boolean) => {
+		if (!open) {
+			closeBook();
+			handleCloseChat();
+		}
+	};
+
+	return {
+		activeCharacter,
+		analyzeBook,
+		handleChatOpen,
+		handleCloseChat,
+		isAnalyzing,
+		isChatOpen,
+		onOpenChange,
+		openedBook,
+		totalCount,
+	};
 }
