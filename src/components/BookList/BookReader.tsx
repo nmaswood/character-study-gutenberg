8c72d84eecaad9ab814b74279cc0e19edd62ef13
kidusleo <kidusleo@gmail.com>
2024-10-31 16:06:38 +0300

    Updated reader

--- src/components/BookList/BookReader.tsx
+++ src/components/BookList/BookReader.tsx
@@ -20,26 +20,36 @@ export default function BookReader({ bookContent }: { bookContent: string }) {
 		);
 
 	return (
-		<div className="flex flex-col items-start justify-between gap-3 overflow-hidden pt-10 transition-all duration-300 lg:max-w-screen-sm">
-			<ScrollArea
-				className={`max-h-[524px] overflow-y-auto whitespace-pre-wrap rounded-md border p-4 text-sm ${courier.className}`}
-			>
-				<div ref={pageStartRef} />
+		<div className="w-full">
+			<div className="flex w-full flex-col items-center justify-between gap-3 overflow-hidden pt-10 transition-all duration-300 md:items-start">
+				<div className="flex w-full flex-row items-center justify-center gap-3">
+					<Button type="button" className="hidden h-8 w-2 md:inline" onClick={prevPage}>
+						<ChevronLeft />
+					</Button>
+					<ScrollArea
+						className={`h-[250px] overflow-y-auto whitespace-pre-wrap rounded-md border p-3 text-xs md:h-[550px] md:w-5/6 md:p-4 md:text-sm ${courier.className} bg-slate-800`}
+					>
+						<div ref={pageStartRef} />
 
-				{currentPage}
-			</ScrollArea>
-			<div className="flex w-full flex-row items-center justify-center gap-3">
-				<Button type="button" onClick={prevPage}>
-					<ChevronLeft />
-				</Button>
-				<div>Page {currentPageNumber + 1}</div>
-				<Button type="button" onClick={nextPage}>
-					<ChevronRight />
+						{currentPage}
+					</ScrollArea>
+					<Button type="button" className="hidden h-8 w-2 md:inline" onClick={nextPage}>
+						<ChevronRight />
+					</Button>
+				</div>
+				<div className="flex w-full flex-row items-center justify-center gap-3 md:hidden">
+					<Button type="button" onClick={prevPage}>
+						<ChevronLeft />
+					</Button>
+					<div>Page {currentPageNumber + 1}</div>
+					<Button type="button" onClick={nextPage}>
+						<ChevronRight />
+					</Button>
+				</div>
+				<Button variant={"outline"} onClick={() => handleReaderToggle()}>
+					Close
 				</Button>
 			</div>
-			<Button variant={"secondary"} onClick={() => handleReaderToggle()}>
-				Close Book
-			</Button>
 		</div>
 	);
 }
